6e1eab2158e8c959abb08abd7fd16f7c3be90ac9
unknown <judemin2087@naver.com>
2024-08-19 20:21:08 +0900

    [INIT] : Init Spring BE Project

---
+++ src/test/java/org/example/whenwillwemeet/WhenWillWeMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.whenwillwemeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhenWillWeMeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
