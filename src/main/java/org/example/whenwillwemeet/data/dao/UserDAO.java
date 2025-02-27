504e05d3d73588efd53ef2698aa7b73931af4613
kangchanwook <kan0202@naver.com>
2025-02-27 14:44:50 +0900

    [FEAT]: Login, Signup API V2 구현을 위한 UserDAO에 매소드 추가

--- src/main/java/org/example/whenwillwemeet/data/dao/UserDAO.java
+++ src/main/java/org/example/whenwillwemeet/data/dao/UserDAO.java
@@ -1,9 +1,15 @@
 package org.example.whenwillwemeet.data.dao;
 
 import com.mongodb.client.result.UpdateResult;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
+import org.example.whenwillwemeet.common.exception.ApplicationException;
+import org.example.whenwillwemeet.common.exception.ErrorCode;
 import org.example.whenwillwemeet.data.model.User;
+import org.example.whenwillwemeet.data.model.UserModel;
+import org.example.whenwillwemeet.data.repository.UserRepository;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -16,32 +22,39 @@ import java.util.Date;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class UserDAO {
-    private final MongoTemplate mongoTemplate;
 
-    public UserDAO(MongoTemplate mongoTemplate) {
-        this.mongoTemplate = mongoTemplate;
+  private final MongoTemplate mongoTemplate;
+  private final UserRepository userRepository;
+
+  public void save(UserModel userModel) {
+    userRepository.save(userModel);
+  }
+
+  public Optional<UserModel> findByEmail(String email) {
+    return userRepository.findByEmail(email);
+  }
+
+  public boolean updateUserInAppointment(String appointmentId, User user) {
+    try {
+      Query query = new Query(Criteria.where("_id").is(appointmentId)
+          .and("users._id").is(user.getName()));
+
+      Update update = new Update()
+          .set("users.$[usr].email", user.getEmail())
+          .set("users.$[usr].phoneNumber", user.getPhoneNumber());
+
+      update.filterArray(Criteria.where("usr._id").is(user.getName()));
+
+      UpdateResult result = mongoTemplate.updateFirst(query, update, "appointments");
+
+      log.info("[UserDAO]-[updateUserInAppointment] Update result: {}", result);
+
+      return result.getModifiedCount() > 0;
+    } catch (Exception e) {
+      log.error("Error updating user in appointment: ", e);
+      return false;
     }
-
-    public boolean updateUserInAppointment(String appointmentId,  User user) {
-        try {
-            Query query = new Query(Criteria.where("_id").is(appointmentId)
-                    .and("users._id").is(user.getName()));
-
-            Update update = new Update()
-                    .set("users.$[usr].email", user.getEmail())
-                    .set("users.$[usr].phoneNumber", user.getPhoneNumber());
-
-            update.filterArray(Criteria.where("usr._id").is(user.getName()));
-
-            UpdateResult result = mongoTemplate.updateFirst(query, update, "appointments");
-
-            log.info("[UserDAO]-[updateUserInAppointment] Update result: {}", result);
-
-            return result.getModifiedCount() > 0;
-        } catch (Exception e) {
-            log.error("Error updating user in appointment: ", e);
-            return false;
-        }
-    }
+  }
 }
