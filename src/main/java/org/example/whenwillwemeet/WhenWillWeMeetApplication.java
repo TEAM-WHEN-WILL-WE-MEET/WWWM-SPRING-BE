289c515f128142bcec5a49d9219c98a7a212dbab
chanwookK <kan0202@naver.com>
2025-07-07 23:49:34 +0900

    [Chore] MongoDB 관련 어노테이션 삭제
    @EnableMongoAuditing
    @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
    제거

--- src/main/java/org/example/whenwillwemeet/WhenWillWeMeetApplication.java
+++ src/main/java/org/example/whenwillwemeet/WhenWillWeMeetApplication.java
@@ -2,11 +2,10 @@ package org.example.whenwillwemeet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableMongoAuditing
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableJpaAuditing
+@SpringBootApplication
 public class WhenWillWeMeetApplication {
 
 	public static void main(String[] args) {
