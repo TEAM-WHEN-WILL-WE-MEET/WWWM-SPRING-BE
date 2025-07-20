47cf49935992a3c4b22d9c8d19351a99cf24d609
chanwookK <kan0202@naver.com>
2025-07-21 00:04:33 +0900

    [Feat] User Read API 에서 provider도 제공하도록 변경

--- src/main/java/org/example/whenwillwemeet/data/dto/UserInfoDto.java
+++ src/main/java/org/example/whenwillwemeet/data/dto/UserInfoDto.java
@@ -1,19 +1,22 @@
 package org.example.whenwillwemeet.data.dto;
 
 import lombok.Builder;
+import org.example.whenwillwemeet.data.enumerate.AuthProvider;
 import org.example.whenwillwemeet.domain.entity.User;
 
 @Builder
 public record UserInfoDto(
     String userId,
     String name,
-    String email
+    String email,
+    AuthProvider provider
 ) {
   public static UserInfoDto of(User user) {
     return UserInfoDto.builder()
         .userId(user.getId().toString())
         .name(user.getName())
         .email(user.getEmail())
+        .provider(user.getProvider())
         .build();
   }
 }
